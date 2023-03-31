4f6c589c681351fcc3f976dbde49a27846afeaab
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-03-31 17:39:57 -0400

    Update random_test_generator.py

--- first_problem/random_test_generator.py
+++ first_problem/random_test_generator.py
@@ -7,26 +7,26 @@ def generate_test_cases(n, min_players, max_players, min_specters, max_specters,
     test_cases = []
     
     for i in range(100):
-        n = 30
-        players_count = np.random.randint(min_players,max_players)
+        players_count = np.random.randint(min_players,max_players, dtype=int)
         specters_count = np.random.randint(min_specters,max_specters)
-        specters_points = np.random.randint(min_score, max_score, size=n).tolist()
+        specters_points = np.random.randint(min_score, max_score, size=n, dtype=int).tolist()
         # print(specters_points, 'point rcs' '\n')
-        players_points = np.random.randint(min_score, max_score, size=n*players_count)
-        players_points = players_points.reshape(n,players_count).tolist()
+        players_points = np.random.randint(min_score, max_score, size=n*players_count, dtype=int)
+        players_points = players_points.reshape(players_count,n).tolist()
         # print(players_points, 'kkk')
-        sol = brute_force_sol(n,players_count,specters_count,players_points,specters_points)
+        # sol = brute_force_sol(n,players_count,specters_count,players_points,specters_points)
         test_cases.append({
+            'n': n,
             'players_count': players_count,
             'specters_count': specters_count,
             'players_points': players_points,
             'specters_points': specters_points,
-            'result' : sol
+            # 'result' : sol
         })
     
     return test_cases
 
-test_cases = generate_test_cases(n=30, min_players=4, max_players=12, min_specters=4, max_specters=20, min_score=1, max_score=100)
+test_cases = generate_test_cases(n=8, min_players=3, max_players=5, min_specters=2, max_specters=3, min_score=1, max_score=20)
 json.dump(test_cases, open('test_cases.json', 'w'))
 
 
