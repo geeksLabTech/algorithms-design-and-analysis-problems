9b907276e3dd7fcf2e9449ab151b20626f6c269b
Lia <liazerqueraf@gmail.com>
2023-05-04 13:04:01 -0400

    modify bruye force from working with evaluator

--- second_problem/brute_force.py
+++ second_problem/brute_force.py
@@ -4,6 +4,8 @@ all_subarr = []
 
 def check(arr: list[int], indexes: list[int]):
     for i in range(len(indexes)-1):
+        if arr[indexes[i]] - arr[indexes[i+1]] == 0: 
+            return True
         if abs(arr[indexes[i]] - arr[indexes[i+1]]) != 1 and 7%(arr[indexes[i]] - arr[indexes[i+1]]) != 1:
             return False
     return True
@@ -25,9 +27,9 @@ def allSubsequences(arr: list[int], index: int, subarr: list[int]):
             all_subarr.append(subarr2)
 
   
-arr = [1, 2, 4,9,7,10,14]
+arr = [1,3,4,8,5,10,5,78,4,2]
 allSubsequences(arr, 0, [])
-print(all_subarr,'aaa')
+# print(all_subarr,'aaa')
 
 def choose_longest(arr_l: list[int]):
     max_len = 0
@@ -41,15 +43,20 @@ def choose_longest(arr_l: list[int]):
                             if len(num_1+num_2+num_3+num_4) > max_len:
                                 max_len = len(num_1+num_2+num_3+num_4)
                                 bests = [num_1, num_2, num_3, num_4]
+    arr.clear()
     return max_len, bests
 
 
-print(choose_longest(all_subarr))
-
 # print(choose_longest(all_subarr))
 
+def execute_brute_force(array):
+    arr = array
+    allSubsequences(arr, 0, [])
+    print(arr, 'arr')
+    return choose_longest(all_subarr)
 
 
+print(execute_brute_force([1,3,4,8,5,10,5,78,4,13]))
 
 
 
